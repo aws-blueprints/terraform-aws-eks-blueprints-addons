155ae5f56c018dc0e8db5f82e96631b7ebe94da3
Bryant Biggs <bryantbiggs@gmail.com>
2025-10-30 15:30:31 -0500

    feat: Upgrade AWS and Helm provider versions to `v6.0` and `v3.1` respectively; upgrade addon submodule to new `v2.0` version

--- tests/complete/versions.tf
+++ tests/complete/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.3.2"
+  required_version = ">= 1.11"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.70, < 6.0"
+      version = ">= 6.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.9, < 3.0"
+      version = ">= 3.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
